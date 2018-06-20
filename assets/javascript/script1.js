139e60a7f6aa29b0f3ceb117c9e8a88c9ac2bdff
Navstar Temp <navtemp@NavstarInc.Net>
2018-06-20 19:17:01 -0400

    HW6

--- assets/javascript/script1.js
+++ assets/javascript/script1.js
@@ -1,13 +1,12 @@
-$ (document).ready(function() {
+$(document).ready(function() {
 
-var gifCategories = ["Daenerys Targaryen", "Jon Snow", "Gregor Clegane", "Cersei Lannister", "Tyrion Lannister", "Arya Stark", "Khal Drogo", "Sansa Stark", "Joffrey Baratheon", "Petyr Baelish", "Sandor Clegane", "Eddard Stark", "Melisandre"];
+var gifCategories = ["happy baby", "sleepy baby", "angry baby", "hungry baby", "sad baby", "silly baby", "scared baby", "dancing baby", "chubby baby", "surprise baby", "walking baby", "crawling baby"];
 var apiKey = "9a7fa4fcadec44039b9827a6100853a9"
 var rating = "pg"
 var searchTerm;
 var newSearchTerm;
 var moving = false;
 
-particlesJS("particles-js", {"particles":{"number":{"value":400,"density":{"enable":true,"value_area":800}},"color":{"value":"#fff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.3367165327817598,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":7.891476416322726,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":500,"color":"#ffffff","opacity":0.4,"width":2},"move":{"enable":true,"speed":6,"direction":"bottom","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"bubble"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":0.5}},"bubble":{"distance":400,"size":4,"duration":0.3,"opacity":1,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
 for (var i = 0; i < gifCategories.length; i++) {
 	$("#buttons").append("<button class='btn btn-secondary' id='gifFilter' value='" + gifCategories[i] + "'>" + gifCategories[i] + "</button>");
 };
@@ -46,6 +45,7 @@ if (moving === true) {
   $(this).attr("src", stillGifURL);
 }
 });
+
 
 function appendStillGifs(response){
         var gifs = response.data; // array of gifs
